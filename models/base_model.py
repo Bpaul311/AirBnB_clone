753ad27d7574491f501b24c76f9275a21ad05ef6
Bpaul311 <bizimapaul777.com>
2024-02-11 22:59:14 -0800

    file storage file added

---
+++ models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a model representing a base through a class"""
+import uuid
+from datetime import datetime
+from models import storage
+
+class BaseModel():
+     """a representation of the base model class"""
+
+
